a9bd8f4008f355567c550b25156cc59283dc4e5a
Dirk van der Laarse <dvanderlaarse@horizonglobal.com>
2019-05-08 22:46:14 +0200

    Try to hide related videos

--- frappe_youtube/api.py
+++ frappe_youtube/api.py
@@ -6,7 +6,7 @@ from frappe import _
 def get_youtube_iframe(youtube_video):
     youtube_video = frappe.get_doc("YouTube Video", youtube_video)
     if youtube_video.video_url is not None:
-        string = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + youtube_video.video_url + '?cc_load_policy=1&modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        string = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + youtube_video.video_url + '?cc_load_policy=1&modestbranding=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     else:
         string = 'No video defined'
     return string
